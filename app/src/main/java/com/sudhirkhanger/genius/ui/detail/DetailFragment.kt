d2c1ddcaa1d7db1f136e6b22b28d791b5fc1d074
Sudhir Khanger <sudhir@sudhirkhanger.com>
2018-09-03 10:34:17 +0530

    move code from detail activity to fragment

---
+++ app/src/main/java/com/sudhirkhanger/genius/ui/detail/DetailFragment.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2018 Sudhir Khanger
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.sudhirkhanger.genius.ui.detail
+
+import android.arch.lifecycle.ViewModelProviders
+import android.databinding.DataBindingUtil
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.sudhirkhanger.genius.R
+import com.sudhirkhanger.genius.databinding.FragmentDetailBinding
+import com.sudhirkhanger.genius.ui.list.MainActivityFragment
+
+class DetailFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val binding: FragmentDetailBinding =
+                DataBindingUtil.inflate(inflater, R.layout.fragment_detail, container, false)
+
+        val movieId = activity?.intent?.extras?.getInt(MainActivityFragment.KEY_MOVIE)
+
+        val detailViewModelFactory = DetailViewModelFactory(movieId!!)
+        val detailViewModel = ViewModelProviders.of(this, detailViewModelFactory).get(
+                DetailViewModel::class.java)
+
+        binding.setLifecycleOwner(this)
+
+        binding.viewModel = detailViewModel
+
+        return binding.root
+    }
+}
